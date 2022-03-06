e4513852738e2949d0beee2b972ceceafd7aa440
Andrés Cataño <3866@holbertonschool.com>
2022-03-05 22:02:51 -0500

    BaseModel Class

---
+++ models/base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Contains class BaseModel
+"""
+
+from datetime import datetime
+import models
+import uuid
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+class BaseModel:
+
+    def __init__(self, *args, **kwargs):
+        """ Initialization of the base model class"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if hasattr(self, "created_at") and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            if hasattr(self, "updated_at") and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            models.storage.new(self)
+            models.storage.save()

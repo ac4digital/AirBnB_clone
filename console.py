7171ffd3d1085c91b7554bf4f43a1abdeb01c584
Andrés Cataño <3866@holbertonschool.com>
2022-03-06 20:43:23 -0500

    console.py executable permissions

--- console.py
+++ console.py
@@ -1,22 +1,24 @@
-
 #!/usr/bin/python3
 """ AirBnB Console """
 
 import cmd
+import models
 
 class HBNBCommand(cmd.Cmd):
     """ HBNH console """
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
-        """Quit command to exit the console"""
+        """ Quit command to exit the console """
         return True
     
     def do_EOF(self, arg):
-        """EOF command to exit the console"""
+        """ EOF command to exit the console """
         return True
 
-
+    def emptyline(self):
+        """ Overwriting the emptyline method """
+        return False
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()

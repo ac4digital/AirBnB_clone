b01897fe446a204d05d98aef8a97f212d08191a2
Andrés Cataño <3866@holbertonschool.com>
2022-03-07 10:22:04 -0500

    Fix pycodestyle

--- console.py
+++ console.py
@@ -4,14 +4,16 @@
 import cmd
 import models
 
+
 class HBNBCommand(cmd.Cmd):
     """ HBNH console """
+
     prompt = '(hbnb) '
 
     def do_quit(self, arg):
         """ Quit command to exit the console """
         return True
-    
+
     def do_EOF(self, arg):
         """ EOF command to exit the console """
         return True
@@ -19,6 +21,7 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """ Overwriting the emptyline method """
         return False
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()

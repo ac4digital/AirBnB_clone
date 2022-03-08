949c8b4291f43016b1053bdea7f0d1cdcd639cc3
Andrés Cataño <3866@holbertonschool.com>
2022-03-08 11:02:28 -0500

    Update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -29,8 +29,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            #models.storage.new(self)
-            #models.storage.save()
+            models.storage.new(self)
+            models.storage.save()
 
     def __str__(self):
         """ Prints the string representation of the BaseModel class """
@@ -41,7 +41,7 @@ class BaseModel:
         """ Save and updates the attribute 'updated_at'
         with the current datetime """
         self.updated_at = datetime.now()
-        #models.storage.save()
+        models.storage.save()
 
     def to_dict(self):
         """ Creates and returns a dictionary containing
